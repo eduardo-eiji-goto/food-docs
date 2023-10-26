f20332eef3286a7e11fbdad2823be9d5752abb2f
Eduardo Eiji Goto <eijigot000@gmail.com>
2023-10-25 21:06:50 -0300

    chore: provide component template

---
+++ docs/.vitepress/theme/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from "vue";
+
+const props = defineProps({
+    text: String,
+});
+
+const count = ref(0);
+</script>
+
+<template>
+    <div>
+        <h2>{{ text }}</h2>
+        <button @click="count = count + 1">Count is: {{ count }}</button>
+    </div>
+</template>
+
+<style scoped></style>
